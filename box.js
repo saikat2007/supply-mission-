a32a28c890e584c70f71c722f8d8f29f28855d40
saikat2007 <67913259+saikat2007@users.noreply.github.com>
2020-10-12 17:44:30 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,8 @@
+lass Box extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+     
+    }
+  
+  };
+  
---
+++ plane.js
@@ -0,0 +1,14 @@
+class plane {
+    var plane,speed,weight,thikness
+    car=createSprite(50,200,50,50);
+    
+  speed=random(55,90);
+  weight=random(400,1500);
+  thickness=random(22,83)
+  this.image = loadImage("sprites/plane.png");
+}
+{function keyPressed() {
+  if (keyCode === DOWN_ARROW)
+  Matter.body.setStatic(false) packagebody,False {
+  }
+}
